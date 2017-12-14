a71f670be69b1abca7ec23368b34b33021357e69
bbungsang <821heejin@naver.com>
2017-12-14 16:17:04 +0900

    MySQL 연결, 문제 번호(url)에 따라 다른 쿼리 호출

--- router/main.js
+++ router/main.js
@@ -1,10 +1,28 @@
 module.exports = function (app, fs) {
-    app.get('/', function (req, res) {
-        res.render('index', {
-            title: 'Nonesense Quiz',
-            length: 5
-        });
+    // MySQL 연결
+    var mysql = require('mysql');
+    var config = require('../db/db_info').local;
+    var connection = mysql.createConnection({
+        host: config.host,
+        port: config.port,
+        user: config.user,
+        password: config.password,
+        database: config.database
     });
+
+    app.get('/solve/:id', function (req, res) {
+        var id = req.params.id;
+        var stmt = "SELECT * FROM nonsense_quiz WHERE id=" + id;
+        connection.query(stmt, function (err, result) {
+           if (err) throw err;
+           var result = result[0];
+           res.render('solve', {
+               title: 'Nonesense Quiz',
+               result: result
+           });
+        })
+    });
+
     app.get('/create', function (req, res) {
         res.render('create', {
             title: ""
