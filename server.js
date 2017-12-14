ffabdb443fc53c44a079f8ee37bbb7d3cbcc5a20
bbungsang <821heejin@naver.com>
2017-12-14 13:59:42 +0900

    router 폴더 생성후 서버 연동, HTML 페이지 생성

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+// 라우터 모듈인 main.js를 불러와서 app에 전달
+var router = require('./router/main')(app);
+
+// 서버가 읽을 수 있도록 HTML의 위치를 정의
+app.set('views', __dirname + '/views');
+// 서버가 HTML 렌더링을 할 때, EJS 엔진을 사용하도록 설정
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+// app.use(express.static('public'));
+
 var server = app.listen(3000, function () {
     console.log("Express server has started on port 3000");
-})
-app.get('/', function (req, res) {
-    res.send('Hello World');
 });
